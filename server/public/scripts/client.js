32fba781678dc20203e9f289e6447797b745d2f8
Dev Jana <dev@primeacademy.io>
2018-11-19 16:45:07 -0600

    end of day 1 lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,41 @@
 $( document ).ready( readyNow );
 
+function addSong(){
+    console.log( 'in addSong' );
+    // get user input
+    // package in an object
+    const objectToSend = {
+        artist: $( '#artistIn' ).val(),
+        published: $( '#publishedIn' ).val(),
+        rank: $( '#rankIn' ).val(),
+        track: $( '#trackIn' ).val()
+    } // end objectToSend
+    console.log( 'sending:', objectToSend );
+    // send to server via AJAX
+    $.ajax({
+        method: 'POST',
+        url: '/songs',
+        data: objectToSend
+    }).then( function( response ){
+        console.log( 'back from POST with:', response );
+        getSongs();
+    }).catch( function( err ){
+        console.log( 'error wit POST:', err );
+    }) //end ajax
+} // end addSong
+
+function getSongs(){
+    $.ajax({
+        method: 'GET',
+        url: '/songs'
+    }).then( function( response ){
+        console.log( 'back from GET with:', response );
+    }).catch( function( err ){
+        console.log( 'error getting data:', err );
+    })
+} // end getSongs
+
 function readyNow(){
-    console.log( 'JQ' );
+    $( '#addSongButton' ).on( 'click', addSong );
+    getSongs();
 } // end readynow
--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.listen( port, ( req, res )=>{
 });
 
 // test route
-app.get( '/test', ( req, res )=>{
+app.get( '/songs', ( req, res )=>{
     console.log( '/test GET hit' );
     // create a query
     const queryString = `SELECT * FROM songs;`;
@@ -47,6 +47,20 @@ app.get( '/test', ( req, res )=>{
         console.log( 'error retrieving data:', err );
     }) // end query
 }); // end test route
+
+app.post( '/songs', ( req, res )=>{
+    console.log( 'in /songs POST:', req.body );
+    // create query string
+    const queryString = `INSERT INTO songs (artist, track, rank, published)
+        VALUES ($1, $2, $3, $4);`;
+    pool.query( queryString, [ req.body.artist, req.body.track, req.body.rank, req.body.published ])
+        .then( ()=>{
+            res.sendStatus( 201 );
+        }).catch( ( err )=>{
+            console.log( 'error writing song:', err );
+            res.sendStatus( 500 );
+        }) // end query
+}) //end /songs POST
 
 
 
